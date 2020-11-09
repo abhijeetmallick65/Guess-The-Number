9f9a3cb255aa46dc72ac53991db90b322eacc424
abhijeetmallick65 <abhijeetmallick29@gmail.com>
2020-11-09 21:43:50 +0530

    Complete project

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const Domstring = {
   Guess: document.querySelector('.guess'),
   Message: document.querySelector('.message'),
   Score: document.querySelector('.score'),
-  Body: document.querySelector(body),
+  Body: document.querySelector('body'),
+  Num: document.querySelector('.number'),
+  Again: document.querySelector('.again'),
+  Highscore: document.querySelector('.highscore'),
 };
 
 const SecretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = SecretNumber;
+let highscore = localStorage.getItem('highscore')
+  ? JSON.parse(localStorage.getItem('highscore'))
+  : 0;
+// Domstring.Num.textContent = SecretNumber;
 
+Domstring.Highscore.textContent = highscore;
 Domstring.Guess.addEventListener('click', () => {
   Domstring.Guess.value = '';
 });
@@ -23,27 +30,40 @@ Domstring.Check.addEventListener('click', function () {
       Domstring.Message.textContent = '⛔ No Number !';
     } else if (guess === SecretNumber) {
       Domstring.Message.textContent = '🎉 You Won The Game';
-      Domstring.body.style.backgroundColor = '#60b347';
-    } else if (guess > SecretNumber) {
-      if (score > 1) {
-        Domstring.Message.textContent = '📈 Too High !';
-        score--;
-        Domstring.Score.textContent = score;
-      } else {
-        Domstring.Message.textContent = '💣 You Have Lost The Game !';
-        Domstring.Score.textContent = 0;
+      Domstring.Body.style.backgroundColor = '#60b347';
+      Domstring.Num.style.width = '30rem';
+      Domstring.Check.style.visibility = 'hidden';
+      Domstring.Num.textContent = SecretNumber;
+      if (score > highscore) {
+        Domstring.Highscore.textContent = score;
+        localStorage.setItem('highscore', JSON.stringify(score));
       }
-    } else if (guess < SecretNumber) {
+    } else if (guess !== SecretNumber) {
       if (score > 1) {
-        Domstring.Message.textContent = '📈 Too low !';
+        Domstring.Message.textContent =
+          guess > SecretNumber ? '📈 Too High !' : '📈 Too low !';
         score--;
         Domstring.Score.textContent = score;
       } else {
         Domstring.Message.textContent = '💣 You Have Lost The Game !';
         Domstring.Score.textContent = 0;
+        Domstring.Check.style.visibility = 'hidden';
       }
     }
   } else {
     window.alert('Enter a number between 1 and 20');
   }
+});
+
+Domstring.Again.addEventListener('click', () => {
+  // location.reload();
+  const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  Domstring.Guess.value = '';
+  Domstring.Score.textContent = score;
+  Domstring.Message.textContent = 'Start guessing...';
+  Domstring.Num.textContent = '?';
+  Domstring.Check.style.visibility = 'visible';
+  Domstring.Body.style.backgroundColor = '#222';
+  Domstring.Num.style.width = '15rem';
 });
